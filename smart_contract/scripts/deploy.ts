aaf4b07a119f5a9117ad4003a944b8ec74a83c3b
Logan Nguyen <nam.p.nguyen131@gmail.com>
2022-07-26 21:25:56 -0500

    Rewrote deploy script

--- smart_contract/scripts/deploy.ts
+++ smart_contract/scripts/deploy.ts
@@ -1,23 +1,14 @@
-import { ethers } from "hardhat";
+import { ethers } from 'hardhat';
 
-async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
+const main = async () => {
+  const Messages = await ethers.getContractFactory('Messages');
+  const messages = await Messages.deploy('Hello, There');
 
-  const lockedAmount = ethers.utils.parseEther("1");
+  await messages.deployed();
+  console.log(`Messages contract deployed to: ${messages.address}`);
+};
 
-  const Lock = await ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
-
-  await lock.deployed();
-
-  console.log("Lock with 1 ETH deployed to:", lock.address);
-}
-
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
-main().catch((error) => {
-  console.error(error);
+main().catch((e: any) => {
+  console.error(e);
   process.exitCode = 1;
 });
