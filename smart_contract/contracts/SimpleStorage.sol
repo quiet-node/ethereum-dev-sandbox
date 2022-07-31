459b4d81d0f3cbb1fa3270eacc4b1fe321d3d457
Logan Nguyen <nam.p.nguyen131@gmail.com>
2022-07-31 08:35:15 -0500

    Create SimpleStorage.sol

---
+++ smart_contract/contracts/SimpleStorage.sol
@@ -0,0 +1,25 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+contract SimpleStorage {
+    uint256 favoriteNumber;
+    struct People {
+        string name;
+        uint256 favoriteNumber;
+    }
+    mapping(string => uint256) nameToFavoriteNumber;
+    People[] people;
+
+    function storeFavoriteNumber(uint256 _favoriteNumber) public {
+        favoriteNumber = _favoriteNumber;
+    }
+
+    function retrieveFavoriteNumber() public view returns (uint256) {
+        return favoriteNumber;
+    }
+
+    function addPerson(string memory _name, uint256 _favoriteNumber) public {
+        people.push(People(_name, _favoriteNumber));
+        nameToFavoriteNumber[_name] = _favoriteNumber;
+    }
+}
