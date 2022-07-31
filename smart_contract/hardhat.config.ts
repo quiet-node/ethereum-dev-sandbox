c50cd9fa9d71aa18452eeb572a817658bb3c2b5e
Logan Nguyen <nam.p.nguyen131@gmail.com>
2022-07-31 09:16:15 -0500

    Added test network to hardhat.config

--- smart_contract/hardhat.config.ts
+++ smart_contract/hardhat.config.ts
@@ -1,8 +1,20 @@
-import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox";
+import { HardhatUserConfig } from 'hardhat/config';
+import '@nomicfoundation/hardhat-toolbox';
+require('dotenv').config();
+
+const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.9",
+  solidity: '0.8.9',
+  networks: {
+    hardhat: {},
+    goerli: {
+      url: GOERLI_RPC_URL,
+      accounts: [PRIVATE_KEY as string],
+      chainId: 420,
+    },
+  },
 };
 
 export default config;
