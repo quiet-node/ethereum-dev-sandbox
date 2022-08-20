c836bdf1776d1978df2b5f1444cbc96005ab1451
Logan Nguyen <nam.p.nguyen131@gmail.com>
2022-08-20 14:48:56 -0400

    Renamed D-00

--- smart_contract/scripts/D-00-Messages.ts
+++ smart_contract/scripts/D-00-Messages.ts
@@ -1,10 +1,15 @@
 import { ethers } from 'hardhat';
 
 const main = async () => {
-  const Messages = await ethers.getContractFactory('Contract00Messages');
+  const Messages = await ethers.getContractFactory('C00Messages');
   const messages = await Messages.deploy('Hello, Messages!');
 
   await messages.deployed();
 
   console.log(`Messages deployed to: ${messages.address}`);
 };
+
+main().catch((e: any) => {
+  console.error(e);
+  process.exitCode = 1;
+});
