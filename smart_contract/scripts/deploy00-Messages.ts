6571723606d8df2a9aefa35fbea0c2a646a1ed91
Logan Nguyen <nam.p.nguyen131@gmail.com>
2022-08-20 14:19:26 -0400

    Update deploy00-Messages.ts

--- smart_contract/scripts/deploy00-Messages.ts
+++ smart_contract/scripts/deploy00-Messages.ts
@@ -1,7 +1,7 @@
 import { ethers } from 'hardhat';
 
 const main = async () => {
-  const Messages = await ethers.getContractFactory('Messages');
+  const Messages = await ethers.getContractFactory('Contract00Messages');
   const messages = await Messages.deploy('Hello, Messages!');
 
   await messages.deployed();
