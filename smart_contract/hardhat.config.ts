6d44b4cc917ab9cbd531cd1d094ff87459ce0cb7
Logan Nguyen <nam.p.nguyen131@gmail.com>
2022-08-21 13:33:02 -0400

    Added hardhat-gas-report to config

--- smart_contract/hardhat.config.ts
+++ smart_contract/hardhat.config.ts
@@ -1,11 +1,13 @@
 import { HardhatUserConfig } from 'hardhat/config';
 import '@nomicfoundation/hardhat-toolbox';
 import '@nomiclabs/hardhat-etherscan';
+import 'hardhat-gas-reporter';
 import 'dotenv/config';
 
 const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL;
 const PRIVATE_KEY = process.env.PRIVATE_KEY;
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY;
 
 const config: HardhatUserConfig = {
   solidity: '0.8.9',
@@ -19,6 +21,12 @@ const config: HardhatUserConfig = {
   },
   etherscan: {
     apiKey: ETHERSCAN_API_KEY,
+  },
+  gasReporter: {
+    enabled: true,
+    currency: 'USD',
+    coinmarketcap: COINMARKETCAP_API_KEY,
+    token: 'ETH',
   },
 };
 
