67ec3b6fe2ecedeb080efd2a6d0d65a2b864c63d
Logan Nguyen <nam.p.nguyen131@gmail.com>
2022-08-21 12:18:16 -0400

    Added C_03

--- smart_contract/contracts/C_03_FixedEtherVault.sol
+++ smart_contract/contracts/C_03_FixedEtherVault.sol
@@ -1,23 +1,25 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 
-
-contract C_03_FixedEtherVault {
+contract C_03_FixedEtherVault is ReentrancyGuard{
     mapping(address => uint256) private balanceOf;
 
     function deposit() external payable{
         balanceOf[msg.sender] += msg.value;
     }
 
-    function withdrawAll() external {
+    function withdrawAll() external nonReentrant{ // FIX: Apply nonReentrant calls from Openzeppelin 
         uint256 userBalance = getAllBalance(msg.sender);
         require(userBalance > 0, "Insufficient Balance!");
+        
+        // FIX: Apply check-effects-interactions pattern
+        balanceOf[msg.sender] = 0; // Update state before call interaction
 
         (bool success, ) = msg.sender.call{value: userBalance}("");
         require(success, "Failed to send ether");
 
-        balanceOf[msg.sender] = 0;
 
     }
 
