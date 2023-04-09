b380ae1d8ace4654650cc1a3699fb75ceeb69f17
Logan <lg.131.dev@gmail.com>
2023-04-08 23:42:21 -0500

    feat: add C_06_Block_Based_Batching

---
+++ smart_contract/contracts/C_06_Block_Based_Batching.sol
@@ -0,0 +1,57 @@
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+contract BatchedTokenSwap {
+    IERC20 public tokenA;
+    IERC20 public tokenB;
+    uint256 public constant BATCH_DURATION = 1 minutes;
+
+    struct SwapRequest {
+        address user;
+        uint256 amount;
+    }
+
+    SwapRequest[] public swapRequests;
+    uint256 public currentBatchEnd;
+
+    event SwapRequestAdded(address indexed user, uint256 amount);
+
+    constructor(IERC20 _tokenA, IERC20 _tokenB) {
+        tokenA = _tokenA;
+        tokenB = _tokenB;
+        currentBatchEnd = block.timestamp + BATCH_DURATION;
+    }
+
+    function requestSwap(uint256 amount) external {
+        require(block.timestamp <= currentBatchEnd, "Batch has ended");
+
+        swapRequests.push(SwapRequest({
+            user: msg.sender,
+            amount: amount
+        }));
+
+        emit SwapRequestAdded(msg.sender, amount);
+    }
+
+    function processBatch() external {
+        require(block.timestamp > currentBatchEnd, "Batch is still ongoing");
+
+        uint256 rate = getRate();
+
+        for (uint256 i = 0; i < swapRequests.length; i++) {
+            SwapRequest storage swapRequest = swapRequests[i];
+
+            require(tokenA.transferFrom(swapRequest.user, address(this), swapRequest.amount), "Transfer failed");
+            require(tokenB.transfer(swapRequest.user, swapRequest.amount * rate), "Transfer failed");
+        }
+
+        delete swapRequests;
+        currentBatchEnd = block.timestamp + BATCH_DURATION;
+    }
+
+    function getRate() public view returns (uint256) {
+        // simplified calculation based on liquidity pools, etc.
+        return 100;
+    }
+}
